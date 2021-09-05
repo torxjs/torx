9c530a27f665363dfe6945feea021714355fc67e
Stephen <slulego@live.com>
2021-09-04 21:14:59 -0400

    Organize files

--- bin/torx.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.compile = void 0;
-function compile(text) {
-    return new Promise(function (resolve, reject) {
-        resolve(text);
-    });
-}
-exports.compile = compile;
