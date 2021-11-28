ae6ca6ad01d18e2eb284c57e68e95e144073b351
Stephen <slulego@live.com>
2021-11-28 18:22:14 -0500

    Simplify eslint and prettier

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,49 +1,49 @@
 import { compile } from "./index";
 
 describe("compile", () => {
-  describe("implicit", () => {
-    it("@title", async () => {
-      const template = "@title";
-      const data = { title: "My Title" };
-      const result = data.title;
-      expect(compile(template, data)).resolves.toEqual(result);
-    });
+   describe("implicit", () => {
+      it("@title", async () => {
+         const template = "@title";
+         const data = { title: "My Title" };
+         const result = data.title;
+         expect(compile(template, data)).resolves.toEqual(result);
+      });
 
-    it("<h1>@title<h1>", () => {
-      const template = "<h1>@title<h1>";
-      const data = { title: "My Title" };
-      const result = `<h1>${data.title}<h1>`;
-      expect(compile(template, data)).resolves.toEqual(result);
-    });
+      it("<h1>@title<h1>", () => {
+         const template = "<h1>@title<h1>";
+         const data = { title: "My Title" };
+         const result = `<h1>${data.title}<h1>`;
+         expect(compile(template, data)).resolves.toEqual(result);
+      });
 
-    it("@getTitle()", () => {
-      const template = "@getTitle()";
-      const data = { getTitle: () => "My Title" };
-      const result = data.getTitle();
-      expect(compile(template, data)).resolves.toEqual(result);
-    });
+      it("@getTitle()", () => {
+         const template = "@getTitle()";
+         const data = { getTitle: () => "My Title" };
+         const result = data.getTitle();
+         expect(compile(template, data)).resolves.toEqual(result);
+      });
 
-    it("@caps('My Title')", () => {
-      const template = "@caps('My Title')";
-      const data = { caps: (param: string) => param.toUpperCase() };
-      const result = "MY TITLE";
-      expect(compile(template, data)).resolves.toEqual(result);
-    });
-  });
+      it("@caps('My Title')", () => {
+         const template = "@caps('My Title')";
+         const data = { caps: (param: string) => param.toUpperCase() };
+         const result = "MY TITLE";
+         expect(compile(template, data)).resolves.toEqual(result);
+      });
+   });
 
-  describe("explicit", () => {
-    it("@(title)", async () => {
-      const template = "@(title)";
-      const data = { title: "My Title" };
-      const result = data.title;
-      expect(compile(template, data)).resolves.toEqual(result);
-    });
+   describe("explicit", () => {
+      it("@(title)", async () => {
+         const template = "@(title)";
+         const data = { title: "My Title" };
+         const result = data.title;
+         expect(compile(template, data)).resolves.toEqual(result);
+      });
 
-    it("<h1>@(title)<h1>", () => {
-      const template = "<h1>@(title)<h1>";
-      const data = { title: "My Title" };
-      const result = `<h1>${data.title}<h1>`;
-      expect(compile(template, data)).resolves.toEqual(result);
-    });
-  });
+      it("<h1>@(title)<h1>", () => {
+         const template = "<h1>@(title)<h1>";
+         const data = { title: "My Title" };
+         const result = `<h1>${data.title}<h1>`;
+         expect(compile(template, data)).resolves.toEqual(result);
+      });
+   });
 });
