35bad73c3faf571fa26953f35be1cfec11743f56
Stephen <slulego@live.com>
2023-05-13 08:43:48 -0400

    Remove dist from git

--- dist/cli.d.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-/**
- * Generate the out path based on two arguments
- * @param arg1 - file path ending in ".torx"
- * @param arg2 - out file path or file extension
- */
-export declare function getOutPath(arg1: string, arg2?: string): string;
